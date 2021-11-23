b84fc11fd0dd953218eee258773dd5252fc0da31
davidbowes818 <davidrb2017@outlook.com>
2021-11-23 01:14:43 +0000

    Font change

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,37 +5,50 @@ import * as indexStyles from "../css/index.module.css"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
-const IndexPage = () => (
-  <nav id={indexStyles.mainNav}>
-    <div className={containerStyles.container}>
-      <div
-        className={containerStyles.row}
-        style={{ alignItems: "center", justifyContent: "space-between" }}
-      >
-        <StaticImage src={"../img/decor.png"} alt="logo" id="main-nav-logo" />
+const IndexPage = ({ children }) => (
+  <div>
+    <nav id={indexStyles.mainNav}>
+      <div className={containerStyles.container}>
+        <div
+          className={containerStyles.row}
+          style={{ alignItems: "center", justifyContent: "space-between" }}
+        >
+          <StaticImage src={"../img/decor.png"} alt="logo" id="main-nav-logo" />
 
-        <ul id={indexStyles.mainNavItems}>
-          <li>
-            <Link className="current" to={"/"}>
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link to={"/products"}>Products</Link>
-          </li>
-          <li>
-            <Link to={"/info"}>Info</Link>
-          </li>
-          <li>
-            <Link to={"/gallery"}>Gallery</Link>
-          </li>
-          <li>
-            <Link to={"/contact"}>Contact Us</Link>
-          </li>
-        </ul>
+          <ul id={indexStyles.mainNavItems}>
+            <li>
+              <Link className="current" to={"/"}>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to={"/products"}>Products</Link>
+            </li>
+            <li>
+              <Link to={"/info"}>Info</Link>
+            </li>
+            <li>
+              <Link to={"/gallery"}>Gallery</Link>
+            </li>
+            <li>
+              <Link to={"/contact"}>Contact Us</Link>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-  </nav>
+    </nav>
+    {children}
+    <footer id={indexStyles.footerMain}>
+      <StaticImage
+        src={"../img/home_decor_footer_logo.png"}
+        alt="footer-logo"
+      />
+      <hr className={indexStyles.footerLine} />
+      <p className={indexStyles.footerNote}>
+        &copy; {new Date().getFullYear()} Decor
+      </p>
+    </footer>
+  </div>
 )
 
 export default IndexPage
